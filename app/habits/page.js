31780d5d3b2c43087064e3341efb39da9a70b00a
Lizwe Mangobe <lizwemjr@icloud.com>
2026-06-06 20:08:39 -0230

    Fix Navbar import path and add key prop to habits list

--- app/habits/page.js
+++ app/habits/page.js
@@ -2,6 +2,16 @@ export default function Habits() {
   return (
     <div>
       <h1>Habits</h1>
+
+      <h2>Health</h2>
+      {/* list of health habits here */}
+
+      <h2>Learning</h2>
+      {/* list of learning habits here */}
+
+      <h2>Fitness</h2>
+      {/* list of fitness habits here */}
+
     </div>
   );
 }
