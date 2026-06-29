81d750687d2e03f9278005cc1784d36d314068df
Lizwe Mangobe <lizwemjr@icloud.com>
2026-06-29 19:37:28 -0230

    Add PUT route for updating habits by ID

--- app/api/habits/[id]/route.js
+++ app/api/habits/[id]/route.js
@@ -1,11 +1,15 @@
 import { connectToDB } from '../../db';
 import { ObjectId } from 'mongodb';
 
-export async function DELETE(request, { params }) {
+export async function PUT(request, { params }) {
     const { db } = await connectToDB();
     const { id } = await params;
+    const data = await request.json();
 
-    await db.collection('habits').deleteOne({ _id: new ObjectId(id) });
+    await db.collection('habits').updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { name: data.name, category: data.category } }
+    );
 
-    return Response.json({ message: 'Habit deleted' });
+    return Response.json({ message: 'Habit updated' });
 }
